a60078a8ff9c5965ae437dae518e8874ea132663
jllaveherr <jllaveherr@educacion.navarra.es>
2022-11-30 20:30:15 +0100

    exam

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+def gas2price(Mes1_Cliente1, Mes2_Cliente1, Mes3_cliente1):
+    """Función que convierta una lista de consumos de gas de diferentes clientes durante tres meses a otra lista de precios a cobrar.
+    Parámetros
+    Entrada: lista con un número indenterminado de tuplas con 3 elementos.
+    Retorno: lista de precios a pagar por cliente.
+    """
+    listas_gas2price = ["Precio_Cliente1", "Precio_Cliente2"]
+
+
+
+
+
---
+++ ejercicio2.py
@@ -0,0 +1,2 @@
+prefijo = {"Grecia":+30, "Francia":+33, "España":+34, "Portugal":+351, "Ucrania":+380, "Italia":+39, "Suiza":+41, "Reino Unido":44, "Alemnia":+49, "Rusia":+7}
+def phone_call():
